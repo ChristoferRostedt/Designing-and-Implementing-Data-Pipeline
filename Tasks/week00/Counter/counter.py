8e1f95933718588e577736a8ce9c1a47c0080f8a
Christofer <christoferrostedt@gmail.com>
2026-01-27 17:16:57 +0200

    Update Structure

---
+++ Tasks/week00/Counter/counter.py
@@ -0,0 +1,16 @@
+class Counter:
+    counter: int
+
+    def __init__(self):
+        self.counter = 0
+
+    def addCount(self) -> None:
+        self.counter += 1
+        return None
+    
+    def getCount(self) -> int:
+        return self.counter
+    
+    def zeroCount(self) -> None:
+        self.counter = 0
+        return None
---
+++ Tasks/week00/Counter/main.py
@@ -0,0 +1,50 @@
+from counter import Counter
+
+def displayOptions() -> None:
+    print("Options:")
+    print("1) Add count")
+    print("2) Get count")
+    print("3) Zero count")
+    print("0) Exit program")
+    return None
+
+def main() -> None:
+    print("Program starting.")
+    print("Initializing counter...")
+    Count = Counter()
+    print("Counter initalized.")
+    print()
+
+    while True:
+        displayOptions()
+        choice = input("Choice: ")
+
+        try:
+            choice = int(choice)
+        except ValueError:
+            print(f"Error value {choice} must be an integer!\n")
+            continue
+
+        if(choice == 1):
+            Count.addCount()
+            print()
+
+        elif(choice == 2):
+            count = Count.getCount()
+            print(f"Current count '{count}'\n")
+
+        elif(choice == 3):
+            Count.zeroCount()
+            print()
+
+        elif(choice == 0):
+            print("Exiting program.\n")
+            break
+
+        else:
+            print("Unknown option!\n")
+
+    return None
+
+if __name__ == "__main__":
+    main()
