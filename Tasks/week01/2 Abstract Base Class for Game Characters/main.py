54f635d5da880293ebe237e420ccfb7ba17e9bfe
Christofer <christoferrostedt@gmail.com>
2026-03-17 11:51:10 +0200

    Update Tasks

--- Tasks/week01/2 Abstract Base Class for Game Characters/main.py
+++ Tasks/week01/2 Abstract Base Class for Game Characters/main.py
@@ -6,6 +6,11 @@ def showMenu() -> None:
     print("0 - Exit")
     return None
 
+def charactersChoices():
+    print("1 - Warrior")
+    print("2 - Mage")
+    print("3 - Archer")
+
 def main() -> None:
     print("Program starting.")
 
@@ -14,7 +19,26 @@ def main() -> None:
         choice = input("Your choice: ")
 
         if(choice == 1):
-            
+            print("choose your character!")
+            charactersChoices()
+            charChoice = input("Choice: ")
+            char = -1
+            while True:
+                try:
+                    if(charChoice == 1):
+                        char = 1
+                        break
+                    elif(charChoice == 2):
+                        char = 2
+                        break
+                    elif(charChoice == 3):
+                        char = 3
+                        break
+                    else:
+                        print("Unknown option. Try again.")
+                except ValueError:
+                    print("Unknown option, try again!")
+
         elif(choice == 2):
             
         elif(choice == 0):
